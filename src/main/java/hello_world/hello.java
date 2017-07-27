c26fde88d83599688dda1af07a03387946674929
Ashwin <ashwin@email.com>
2017-07-27 09:44:03 -0400

    pushing new src & pom.xml with tests

--- src/main/java/hello_world/hello.java
+++ src/main/java/hello_world/hello.java
@@ -1,8 +1,16 @@
+package hello_world;
+
 /**
  * Created by evan on 7/17/17.
  */
 public class hello {
     public static void main(String args[]) {
         System.out.print("hello world/n");
+        hello thingy = new hello();
+        System.out.println(thingy.method(5));
+    }
+
+    public int method(int thing) {
+        return thing*2;
     }
 }
